3ba049114103a37e30489234311b5a68e593983b
The_Boss <73382642+Littleluigi2007@users.noreply.github.com>
2024-08-02 21:24:42 +0300

    Create hakkımdaki_gerçekler.py

---
+++ hakkımdaki_gerçekler.py
@@ -0,0 +1,13 @@
+# Arkadaşlarınızı şaşırtın!
+my_words = ["Hi!", "Python Pro", "Kodland"] 
+print(my_words[1])
+
+name = input()
+print("Hi ", name)
+
+import random
+emojis = ["^^", "0_o", ":)", "¯\\_(ツ)_/¯", "(￢_￢)"]
+print(random.choice(emojis))
+
+ben = ["oyun oynamak", "kedi", "satranç"]
+print(random.choice(ben))
